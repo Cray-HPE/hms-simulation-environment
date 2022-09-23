d77f15e4fc88b1fb184ec770ef1728710fc303b1
Ryan Sjostrand <sjostrand@hpe.com>
2022-09-23 17:12:35 -0500

    Try again

--- runIntegration.sh
+++ runIntegration.sh
@@ -48,6 +48,11 @@ if ! ./run.py configs/sls/small_mountain.json; then
   cleanup 1 
 fi
 
+docker compose ps
+docker compose logs vault
+docker compose logs cray-meds
+exit 1
+
 # Run the smoke tests
 for smoke_test in test-smoke-api-gateway-services test-smoke-api-gateway-hmn test-smoke-api-gateway-rie-proxy; do
     if ! docker-compose -f docker-compose.integration.yaml up --exit-code-from "${smoke_test}" "${smoke_test}"; then
